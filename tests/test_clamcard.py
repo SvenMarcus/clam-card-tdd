237cb4378d9bc21231f81085489629cf10d3889c
Sven Marcus <s.marcus@outlook.de>
2024-02-13 16:50:24 +0100

    Passing same gate twice doesnt charge bank account

--- tests/test_clamcard.py
+++ tests/test_clamcard.py
@@ -10,10 +10,13 @@ class ClamCard:
     def __init__(self, bank_account: BankAccountSpy) -> None:
         self._bank_account = bank_account
         bank_account.charge(0.01)
+        self._last_gate = None
 
     def pass_gate(self, station: str) -> None:
-        if station == "Amersham":
+        if self._last_gate and station != self._last_gate:
             self._bank_account.charge(2.5)
+
+        self._last_gate = station
 
 
 def test__clam_card_linked_to_a_bank_account__charges_a_cent_for_verification() -> None:
@@ -30,5 +33,28 @@ def test__single_journey_in_zone_A__charges_additonal_zone_A_price() -> None:
 
     sut.pass_gate("Asterisk")
     sut.pass_gate("Amersham")
+
+    assert bank_account.charged == 2.51
+
+
+def test__passing_same_gate_in_station__doesnt_charge_additional_zone_A_price() -> None:
+    bank_account = BankAccountSpy()
+    sut = ClamCard(bank_account)
+
+    sut.pass_gate("Asterisk")
+    sut.pass_gate("Asterisk")
+
+    assert bank_account.charged == 0.01
+
+
+def test__passing_same_gate_in_station__doesnt_charge_additional_zone_A_price__() -> (
+    None
+):
+    bank_account = BankAccountSpy()
+    sut = ClamCard(bank_account)
+
+    sut.pass_gate("Amersham")
+    sut.pass_gate("Asterisk")
+    sut.pass_gate("Asterisk")
 
     assert bank_account.charged == 2.51
